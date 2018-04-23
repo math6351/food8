38d1fec1024dc24718b1428e1c612ccea71fefe2
Mathilde Pedersen <mathildepedersen@lenovodeluxe.kea.local>
2018-04-23 11:23:40 +0200

    php

--- sendmail.php
+++ sendmail.php
@@ -7,8 +7,8 @@ $telefon=$_REQUEST['telefon'];
 $cvr=$_REQUEST['cvr'];
 
 $besked=$_REQUEST['besked'];
-$subject="Besked fra" . $navn . ": ". $email;
-$message= $navn . $cvr . $besked . $telefon;
+$subject="Besked fra " . $navn . ": ". $email;
+$message= $navn "\r\n" . $cvr "\r\n" . $besked "\r\n" . $telefon;
 
 $header = "Content-type: text/html; charset=utf-8" . "\r\n";
 $header.="from:ms.oldorf@mathildeoldorf.dk";
