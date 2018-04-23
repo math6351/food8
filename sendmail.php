ecd2bbad6d740cd3a9850c82027492271ed7a18e
Mathilde Pedersen <mathildepedersen@lenovodeluxe.kea.local>
2018-04-23 10:37:52 +0200

    php

--- sendmail.php
+++ sendmail.php
@@ -1,10 +1,13 @@
 <?php
 $to="ms.oldorf@mathildeoldorf.dk";
+
 $navn="Mathilde";
 $emne="Hej";
 $besked="Vil du med i biffen?";
+
 $subject="Besked fra" . $navn . ": ". $to;
-$message="<h1 style='color:blue'>" . $emne . " " . $besked;
+$message="<h1 style='color:blue'>" . $emne . "</h1>" . $besked;
+
 $header = "Content-type: text/html; charset=utf-8" . "\r\n";
 $header.="from:ms.oldorf@gmail.com";
 
