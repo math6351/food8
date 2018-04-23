96f22e80f175d7e6e490d1412fcca356d5e33099
Mathilde Pedersen <mathildepedersen@lenovodeluxe.kea.local>
2018-04-23 10:59:58 +0200

    php

--- sendmail.php
+++ sendmail.php
@@ -1,12 +1,17 @@
 <?php
 $to="ms.oldorf@gmail.com";
-$navn="Mathilde";
-$emne="Hej";
-$besked="Vil du med i biffen?";
+
+$navn=$_REQUEST['navn'];
+$email=$_REQUEST['email'];
+$emne=$_REQUEST['emne'];
+
+$besked=$_REQUEST['besked'];
 $subject="Besked fra" . $navn . ": ". $to;
 $message= "<h1 style='color:red'>" . $emne . "</h1> " . $besked;
+
 $header = "Content-type: text/html; charset=utf-8" . "\r\n";
 $header.="from:ms.oldorf@mathildeoldorf.dk";
 
 mail($to, $subject, $message, $header);
+header("Location: om.html");
 ?>
