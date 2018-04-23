076c5392703152158310f4d88db469eca2343e3d
Mathilde Pedersen <mathildepedersen@lenovodeluxe.kea.local>
2018-04-23 10:48:18 +0200

    php

--- sendmail.php
+++ sendmail.php
@@ -4,8 +4,9 @@ $navn="Mathilde";
 $emne="Hej";
 $besked="Vil du med i biffen?";
 $subject="Besked fra" . $navn . ": ". $email;
-$message=$emne . " " . $besked;
-$header="from:ms.oldorf@gmail.com";
+$message= "<h1 style='color:red'>" . $emne . "</h1> " . $besked;
+$header = "Content-type: text/html; charset=utf-8" . "\r\n";
+$header.="from:ms.oldorf@gmail.com";
 
 mail($to, $subject, $message, $header);
 ?>
